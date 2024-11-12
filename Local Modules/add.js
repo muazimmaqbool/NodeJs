b59b150b90068046274e983d738cb5328fe79226
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-12 21:38:02 +0530

    require definition

--- Local Modules/add.js
+++ Local Modules/add.js
@@ -1,5 +1,6 @@
 //this is an exmaple of local module inside this file we write a function which calculates add of two numbers
-//this function is used in inside index.js
+//this function is used in inside index.js via require function
+//require function: allows us to load module into another file
 
 const add=(a,b)=>{
     return a+b;
