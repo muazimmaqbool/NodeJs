43ee7ca7071a5284d9cc4c077c1da89939beb413
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-12 20:47:19 +0530

    require function using module

---
+++ add.js
@@ -0,0 +1,8 @@
+//this is an exmaple of local module inside this file we write a function which calculates add of two numbers
+//this function is used in inside index.js
+
+const add=(a,b)=>{
+    return a+b;
+}
+const sum=add(6,6);
+console.log(sum)
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log("Aslamualikum");
 //to run this type this in terminal: node index.js or node ./index.js or node .\index.js or node index
+
+//using add.js module here using require function
+require('./add')
+//so here require function loads add.js module into index.js file
