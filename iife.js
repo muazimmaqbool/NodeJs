4104ff67ee8a2c9b710a9ed86a19a60bc0eb4524
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-18 22:37:44 +0530

    module wrapper

--- iife.js
+++ iife.js
@@ -9,3 +9,25 @@
   const superHero = "Superman";
   console.log(message, superHero);
 })("Hey");
+
+//here is the module code inside 3_batman and 4_superman:
+/*
+const superHero="Batman";
+console.log("SuperHero:",superHero)
+*/
+
+//here is the same code wrapped in IIFE function:
+/*
+(function (message) {
+  const superHero = "Batman";
+  console.log(message, superHero);
+})
+*/
+
+//here is the final code with parameters there are 5 in total
+/*
+(function (exports,require,module, __filename,__dirname) {
+  const superHero = "Batman";
+  console.log(message, superHero);
+})
+*/ //now watch #11 at 2:43 
