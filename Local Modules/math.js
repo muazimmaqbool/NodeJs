4e78411680dc2feb3df3ee0623ff946dc5f1c643
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-23 12:03:14 +0530

    Importing/Exporting more than one function/variable

--- Local Modules/math.js
+++ Local Modules/math.js
@@ -5,6 +5,8 @@ const add=(a,b)=>{
     return a+b
 }
 
+//->importing/exporting one variable or function:
+
 //? Ist pattern ( when we have to export and import a single variable of function)
 //module.exports=add;
 
@@ -25,4 +27,34 @@ module.exports =(a,b)=>{
     console.log(add(5,6))
 
     ->to this this comment out first code
+*/
+
+//importing/exporting more than one variable/function
+const sub=(a,b)=>{
+    return(a-b);
+}
+
+//?Now to export both add and sub function do this:
+// module.exports={
+//     add:add,
+//     sub:sub,
+// }
+// or (with ES-2015 features we specify just add and sub if the key and value are the same i.e module.exports={add,sub} )
+module.exports={add,sub} //this is preffered
+
+/*
+import it like this: in index.js file:
+
+    const math=require('./Local Modules/math');
+    console.log(math.add(5,6))
+    console.log(math.sub(5,6))
+
+    ->math can be any name
+    ->Note: instead of using math.add or math.sub we can destructure the math object:
+    (destructuring is again an ES-2015 feature)
+
+    const math=require('./Local Modules/math');
+    const{add,sub}=math; //here we destructure the functions from the object
+    console.log(add(5,6))
+    console.log(sub(5,6))
 */
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 console.log("Aslamualikum");
 //to run this type this in terminal: node index.js or node ./index.js or node .\index.js or node index
 
-const add=require('./Local Modules/math');
+const math=require('./Local Modules/math');
+const{add,sub}=math;
 console.log(add(5,6))
+console.log(sub(5,6))
