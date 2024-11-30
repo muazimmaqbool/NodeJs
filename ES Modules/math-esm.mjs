d077a0234c57d777c4b0786f121ce56b605b29be
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-30 20:53:23 +0530

    es modules importing and export by name

--- ES Modules/math-esm.mjs
+++ ES Modules/math-esm.mjs
@@ -1,8 +1,9 @@
 // we will use this file inside index-esm.mjs
 
-const add=(a,b)=>{
-    return a+b;
-}
+//two make 1 and 2 methos work comment code of mehtod 3
+// const add=(a,b)=>{
+//     return a+b;
+// }
 /*
     or do this : export default(a,b)=>{...}
     but then remove this : export default add;
@@ -14,8 +15,47 @@ const add=(a,b)=>{
     
 
 //2-> exporting multiple  variables/functions
-const sub=(a,b)=>{
+// const sub=(a,b)=>{
+//     return a-b;
+// }
+
+//export default {add,sub}
+/*
+->Import it like this:
+    import math from "./ES Modules/math-esm.mjs"
+->here math is an object and it contains all the functions/variables which are exported
+->here math can be any name
+then use like this : math.sub , math.add 
+ or destructure:
+ const {add,sub}=math
+
+
+*/
+
+//Note all the active code above
+
+/*3-> In es modules we also have what is known as named exports, where the varaible or function name being
+exported must match while importing it.
+so here while defining a function or variable we can add export keyword at the start
+*/
+export const add=(a,b)=>{
+    return a+b;
+}
+export const sub=(a,b)=>{
     return a-b;
 }
 
-export default {add,sub}
+//import then like this:
+/*
+import * as math from "./filename" //math is an object and it contains all the exports
+const {add,sub}=math or math.add, math.sub
+
+or import like this (this is commonly used)
+import {add,sub} from "./ES Modules/math-esm.mjs" 
+->here we are basically destructuring while importing
+console.log("add:",add(10,10));
+console.log("sub:",sub(10,10)); 
+
+
+
+*/
--- index-esm.mjs
+++ index-esm.mjs
@@ -1,17 +1,27 @@
 //this index-esm.mjs file is used to import or run modules which are exported using ES Modules system
 
+
+//-> to make this work comment below code from ->
 //importing a single variable/function
 //import add from "./ES Modules/math-esm.mjs"
 
 //importing multiple variable/function
-import math from "./ES Modules/math-esm.mjs"
+//import * as math from "./ES Modules/math-esm.mjs"
 //here math is an object and it contains all the functions/variables which are exported
+//here math can be any name
 
-console.log("add:",math.add(6,10));
-console.log("sub:",math.sub(6,10));
+//console.log("add:",math.add(6,10));
+//console.log("sub:",math.sub(6,10));
 
 //we can also destructure the variables and functions
-const {add,sub}=math
+//const {add,sub}=math
 
-console.log("add:",add(6,10));
-console.log("sub:",sub(6,10));
+// console.log("add:",add(6,10));
+// console.log("sub:",sub(6,10));
+
+//->to make this method work (comment all above code)
+//importing multiple exports with name
+import {add,sub} from "./ES Modules/math-esm.mjs" 
+//here we are basically destructuring while importing
+console.log("add:",add(10,10));
+console.log("sub:",sub(10,10));
