23248498fd680ac287192f1d25873a33161505eb
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-30 18:49:50 +0530

    exporting and importing a single func using es modules

--- ES Modules/math-esm.mjs
+++ ES Modules/math-esm.mjs
@@ -3,4 +3,11 @@
 const add=(a,b)=>{
     return a+b;
 }
-//1 -> exporting and importing single a variable/function
+//1 -> exporting single a variable/function
+export default add;
+   //import it like this in index-esm.mjs file: import add from "./ES Modules/math-esm.mjs"
+    /*
+    or do this : export default(a,b)=>{...}
+    */
+
+//2-> exporting multiple  variables/functions
--- index-esm.mjs
+++ index-esm.mjs
@@ -1,1 +1,5 @@
 //this index-esm.mjs file is used to import or run modules which are exported using ES Modules system
+
+//importing a single variable/function
+import add from "./ES Modules/math-esm.mjs"
+console.log("add:",add(6,10));
