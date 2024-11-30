5a9b75dba6e94540c9dbd0aaa6c5a0912b5312d9
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-11-30 20:39:02 +0530

    importing and exporting multiple using es modules

--- ES Modules/math-esm.mjs
+++ ES Modules/math-esm.mjs
@@ -3,11 +3,19 @@
 const add=(a,b)=>{
     return a+b;
 }
-//1 -> exporting single a variable/function
-export default add;
-   //import it like this in index-esm.mjs file: import add from "./ES Modules/math-esm.mjs"
-    /*
+/*
     or do this : export default(a,b)=>{...}
+    but then remove this : export default add;
     */
+//1 -> exporting single a variable/function
+//export default add;
+
+   //import it like this in index-esm.mjs file: import add from "./ES Modules/math-esm.mjs"
+    
 
 //2-> exporting multiple  variables/functions
+const sub=(a,b)=>{
+    return a-b;
+}
+
+export default {add,sub}
--- index-esm.mjs
+++ index-esm.mjs
@@ -1,5 +1,17 @@
 //this index-esm.mjs file is used to import or run modules which are exported using ES Modules system
 
 //importing a single variable/function
-import add from "./ES Modules/math-esm.mjs"
+//import add from "./ES Modules/math-esm.mjs"
+
+//importing multiple variable/function
+import math from "./ES Modules/math-esm.mjs"
+//here math is an object and it contains all the functions/variables which are exported
+
+console.log("add:",math.add(6,10));
+console.log("sub:",math.sub(6,10));
+
+//we can also destructure the variables and functions
+const {add,sub}=math
+
 console.log("add:",add(6,10));
+console.log("sub:",sub(6,10));
