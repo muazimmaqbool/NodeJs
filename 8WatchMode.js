928ef5e8b585f15ec9fd7da9a9ad4622b0388a37
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-08 22:01:19 +0530

    Json importing and watchmode

---
+++ 8WatchMode.js
@@ -0,0 +1,14 @@
+console.log("Watch Mode in Node.js")
+/*
+->Watch mode in Node. js allows you to automatically restart your server whenever you make changes to your code, 
+  streamlining the development process. 
+  This eliminates the need to manually restart the server after every code modification, saving you time and effort. 
+  
+->Introduced as an experimental feature in Node.js version 18.11.0, watch mode is now available.
+  
+    ->Use the --watch flag with the node command followed by the filename of your main server file.
+    ->Example: node --watch index.js
+
+    ->To Exit watch mode press ctrl + c
+*/
+console.log("Introduced in Nodel.js version 18.11.0")
