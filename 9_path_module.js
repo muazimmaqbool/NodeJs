591a9e846a2fa6d9bef55e913b0d602ad5961671
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-12-22 12:04:52 +0530

    path's basename method

--- 9_path_module.js
+++ 9_path_module.js
@@ -7,10 +7,19 @@ const path=require("node:path"); //here we prefix it with node: to indicate that
 //Note: the path module have 14 different methods but we will focus on 7 which are regularly used
 
 //Note: we will also use __filename and __dirname these are available in every module
-console.log("__filename:",__filename) //represents the full path to the current file
-console.log("__direname:",__dirname) //represents the full path to the current folder
+//console.log("__filename:",__filename) //represents the full path to the current file
+//console.log("__direname:",__dirname) //represents the full path to the current folder
 /*
 o/p:
 __filename: C:\Users\Muazim\Documents\NodeJs\9_path_module.js
 __direname: C:\Users\Muazim\Documents\NodeJs
 */
+
+//Different methods of path module:
+/*
+1)basename: it returns the last portion of the path
+example:
+*/
+console.log("path.basename():")
+console.log(path.basename(__filename)) // o/p: 9_path_module.js
+console.log(path.basename(__dirname)) // o/p: NodeJs
