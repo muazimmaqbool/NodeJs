2b15bb72da252434c158b6af0dfcdffd1077c58d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-19 22:14:50 +0530

    third party module info

--- 4.3ThirdPartyModules.js
+++ 4.3ThirdPartyModules.js
@@ -1,7 +1,7 @@
 /*Third Party Modules:
-    ->these modules are written by someone else but we can download them and use them in our project
-    ->these modules can be installed in the project folder or globally
-    ->example of best thrid party modules: express, gulp, lodash, async, scoket.io, mongoose, 
+    ->These modules are written by someone else but we can download them and use them in our project
+    ->These modules can be installed in the project folder or globally
+    ->Example of best thrid party modules: express, gulp, lodash, async, scoket.io, mongoose, 
     pokemon, underscore, pm2, bower, q, debug, react, mocha etc
         */
 //as an example we will use pokemon third paty module:
