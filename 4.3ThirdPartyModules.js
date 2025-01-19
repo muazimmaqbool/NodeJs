c53620ba8287b8713de2f2c5bc38c61d86a66edc
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-19 22:16:53 +0530

    third party module example

--- 4.3ThirdPartyModules.js
+++ 4.3ThirdPartyModules.js
@@ -2,8 +2,8 @@
     ->These modules are written by someone else but we can download them and use them in our project
     ->These modules can be installed in the project folder or globally
     ->Example of best thrid party modules: express, gulp, lodash, async, scoket.io, mongoose, 
-    pokemon, underscore, pm2, bower, q, debug, react, mocha etc
-        */
+                                           pokemon, underscore, pm2, bower, q, debug, react, mocha etc
+*/
 //as an example we will use pokemon third paty module:
 //first download it : npm i pokemon
 const pokemon=require("pokemon");
@@ -11,7 +11,6 @@ console.log(pokemon.random()); //generates any random pokemon name
 //console.log(pokemon.all()); //prints all pokemon names
 
 
-//at 44:00
 //there is one more package nodemon: it restarts code everytime we save the code
 //so, to run our code everytime we have to enter c in terminal with the
 //help of nodemon just once we have to type nodemon 'filename'
