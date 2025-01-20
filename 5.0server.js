4eb199ba0b44238dcfe26a18c10a2584a62ce13d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-20 21:49:09 +0530

    Example

--- 5.0server.js
+++ 5.0server.js
@@ -11,7 +11,8 @@ const http = require("http");
   these names can be anything like : r,a,n-> i.e first paramter is always request, then response, then next
   */
 const server=http.createServer((request, response) => {
-    response.end("sever is working fine") //will be shown on screen
+    response.end("sever is working fine") 
+    //will be shown on screen
 });
 
 //important
@@ -21,4 +22,3 @@ server.listen(2000,"localhost",()=>{
 //hold and click on localhost:2000 in terminal it will open in browser
 
 //we can write it in better way
-//see code 5.1server.js
