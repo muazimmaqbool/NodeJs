be580589a5bca8add8a9ffc8df7c65557fb24977
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-21 22:28:06 +0530

    json data print

--- 7_RUNJSON.js
+++ 7_RUNJSON.js
@@ -1,12 +1,12 @@
 /*
  #17 importing json and watch mode
--> This file is used to run and show json data 7_Data.json  file
+  -> This file is used to run and show json data which is inside 7_Data.json  file
 */
 
 /*
  Note:if you write 7_Data only then nodejs will first try to fint 7_Data.js before trying to find 7_Data.json
- So, if there is an 7_Data.js file in you folder then node.js will run it and not the actual json file so, its better you use 
- name with extension also like this filename.json 
+ So, if there is an 7_Data.js file in you folder then node.js will run it and will not run the actual json file so, 
+ its better you use name with extension also like this filename.json 
 */
 const data=require("./7_Data.json") // or const data=require("./7_Data")
 console.log(data)
@@ -17,3 +17,4 @@ o/p: { name: 'Batman', address: { street: 'Wayne Manor', city: 'Gotham' } }
     we can access name like this: data.name, etc
 */
 console.log("name:",data.name)
+console.log("address:",data.address)
