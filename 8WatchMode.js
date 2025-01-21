fafcf619646f57ca59f2dd552fcef2430e9c75af
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-21 22:29:58 +0530

    watc mode

--- 8WatchMode.js
+++ 8WatchMode.js
@@ -1,10 +1,11 @@
 console.log("Watch Mode in Node.js")
 /*
-->Watch mode in Node. js allows you to automatically restart your server whenever you make changes to your code, 
-  streamlining the development process. 
+->Watch mode in Node.js allows you to automatically restart your server whenever you make changes to your code, 
+  it helps streamlining the development process. 
   This eliminates the need to manually restart the server after every code modification, saving you time and effort. 
   
-->Introduced as an experimental feature in Node.js version 18.11.0, watch mode is now available.
+->It was introduced as an experimental feature in Node.js version 18.11.0, 
+    watch mode is now available.
   
     ->Use the --watch flag with the node command followed by the filename of your main server file.
     ->Example: node --watch index.js
