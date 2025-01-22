26ecaf6e552a512a758a2b8a4c301b826b6e91f1
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-22 21:32:24 +0530

    filename and dirname

--- 9_path_module.js
+++ 9_path_module.js
@@ -18,8 +18,8 @@ const path=require("node:path"); //here we prefix it with node: to indicate that
 //Note: the path module have 14 different methods but we will focus on 7 which are regularly used
 
 //Note: we will also use __filename and __dirname these are available in every module
-//console.log("__filename:",__filename) //represents the full path to the current file
-//console.log("__direname:",__dirname) //represents the full path to the current folder
+console.log("__filename:",__filename) //represents the full path to the current file
+console.log("__direname:",__dirname) //represents the full path to the current folder
 /*
 o/p:
 __filename: C:\Users\Muazim\Documents\NodeJs\9_path_module.js
