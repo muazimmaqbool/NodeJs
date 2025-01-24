c1ef38412f0fa54c60b8325c63adbd4bfd3616ec
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-01-24 21:06:20 +0530

    fix

--- 10_events_module.js
+++ 10_events_module.js
@@ -1,6 +1,6 @@
 /*
 Event Module in Node.js:
-    ->Node.js has a built-in module, called "Events", where you can create-, fire-, and listen for- your own events.
+    ->Node.js has a built-in module, called "Events", where you can create, fire, and listen your own events.
     ->The event module allows us to work with events in node.js
         {event: its an action or occurrence that has happened in our application that we can respond to}
     ->using Event Module, we can dispatch our own custom events and respoont to those custom events in a non-blocking manner
