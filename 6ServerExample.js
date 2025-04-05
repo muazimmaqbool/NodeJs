50229a13d05dec3660aca38cb6c593a92d321278
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-05 10:53:56 +0530

    html pages loaded via fs and sever

--- 6ServerExample.js
+++ 6ServerExample.js
@@ -1,13 +1,16 @@
-//here we will load file on display
-//creating files html for home,about and contact to display them using file system and server on web browser
+//first see previous codes: 5.0server.js, 5.1server.js and 5.2server.js
+
+//here we will load file on display:
+//Creating html pages for home, about and contact to display them using file system and server on web browser
 //These files are inside HtmlPages folder
 
-const http = require("http");
-const fs=require("fs");
+const http = require("http"); //used to work with servers
+const fs=require("fs"); //used to work with files
 
 const port = 5000;
 const hostname = "localhost";
-//reading html files in synchronous mode
+//reading html files in synchronous mode 
+//in synchronous mode because i want the files to load first
 const home=fs.readFileSync('./HtmlPages/home.html',"utf-8");
 const about=fs.readFileSync('./HtmlPages/about.html',"utf-8");
 const contact=fs.readFileSync('./HtmlPages/contact.html',"utf-8");
