911ceedf437603f46aa5434693ccdc570616bd9e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-05 10:36:15 +0530

    server example 1

--- 5.0server.js
+++ 5.0server.js
@@ -5,11 +5,11 @@ const http = require("http");
 /* 
   http.createServer((request,response,next)=>{...}
 
-  -> 'request' is  looking for something like search data in google i.e request and google gives o/p
+->'request' is  looking for something like search data in google i.e request and google gives o/p
   that's the 'response' and next is callback function for now we don't need it it's generally required
   for complex projects where we have multiple routes etc (so we have removed 'next' for now)
   these names can be anything like : r,a,n-> i.e first paramter is always request, then response, then next
-  */
+*/
 const server=http.createServer((request, response) => {
     response.end("sever is working fine") //will be shown on screen
 });
@@ -18,6 +18,6 @@ const server=http.createServer((request, response) => {
 server.listen(2000,"localhost",()=>{
     console.log("server is working on http://localhost:2000")
 })
-//hold and click on localhost:2000 in terminal it will open in browser
+//hold and click on http://localhost:2000 in terminal it will open in browser
 
 //we can write it in better way see next code 5.1server.js
