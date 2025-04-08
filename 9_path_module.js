e0b46d928673dd8c2738cfbf90a82112186b8eb8
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-08 18:49:08 +0530

    path methods

--- 9_path_module.js
+++ 9_path_module.js
@@ -18,6 +18,7 @@ const path=require("node:path"); //here we prefix it with node: to indicate that
 //Note: the path module have 14 different methods but we will focus on 7 which are regularly used
 
 //Note: we will also use __filename and __dirname these are available in every module
+//and didn't need to be imported
 console.log("__filename:",__filename) //represents the full path to the current file
 console.log("__direname:",__dirname) //represents the full path to the current folder
 /*
@@ -44,7 +45,7 @@ console.log(path.extname(__filename)) // o/p: .js
 console.log(path.extname(__dirname)) // o/p: no output i.e empty string since it dosn't have . character in its path
 
 /*
-3) parse method: it returns an object whose properties represent significant elements of the path:
+3)parse method: it returns an object whose properties represent significant elements of the path:
    like root, dir,base, ext, name  of the path
 */
 console.log("path.parse():")
