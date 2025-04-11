c072c073d120fdf7a370f617947137d100999f4e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-11 20:29:16 +0530

    module chaching

--- Local Modules/moduleCache.js
+++ Local Modules/moduleCache.js
@@ -1,5 +1,5 @@
 /*
- this is file which is in run_Module_Cache.js file, to explain to you what is "Module Caching"
+ this is the file which is used in run_Module_Cache.js file, to explain to you what is "Module Caching"
  source: https://medium.com/@interviewer.live/understanding-module-caching-in-node-js-977dab968541#:~:text=Module%20caching%20is%20a%20mechanism,module%20has%20already%20been%20loaded.
 */
 console.log("Module Cache file is loaded")
