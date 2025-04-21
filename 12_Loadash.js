27ff06f1a6ce17300800687e090cfc8ae5fa9e00
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-21 09:32:28 +0530

    Lodash example

--- 12_Loadash.js
+++ 12_Loadash.js
@@ -6,5 +6,22 @@
      This in turn can help increase code efficiency and make code easier to read.
     
   - Installing loadash: npm i loadash
-  - Docs: 
+  - Docs: https://lodash.com/ or https://lodash.com/docs/4.17.15
 */
+//importing loadash
+var _ =require("lodash")
+/*
+->why we used imported it using underscore"_" here, infact we can use any name and that will work fine eg. var myLoadash=require("lodash")
+    we use underscore because its used everywhere like every developer used it like this and also lodash docs also user underscore
+*/
+
+//Example
+var data=["Laptop","Hp","Mobile","hp",1,"hp",2,1,1,1,2,"Laptop"]
+/*From the above data array i wanted to print the unique elements from it and with the help of lodash it can be done quickly
+  using it various methods and if we don't use lodash it will take us time to write logic and lots of calculations
+
+  Loadash have lots of methods (see docs) that are used to deal with the data and it reduces lot of work
+*/
+console.log("data:",data)
+const filterValues=_.uniq(data)
+console.log("Getting Unique Elements from data:",filterValues)
