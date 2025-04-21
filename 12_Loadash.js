017be2b6c403a49e2d9d3aa8d499ce51ddbad209
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-21 09:21:48 +0530

    loadash info

---
+++ 12_Loadash.js
@@ -0,0 +1,10 @@
+/*
+->Loadash Package:
+  -Lodash makes JavaScript easier by taking the hassle out of working with arrays, numbers, objects, strings, etc. 
+    Lodash's modular methods are great for: Iterating arrays, objects, & strings. Manipulating & testing values.
+  -It provides a wide range of helper functions to simplify common programming tasks.
+     This in turn can help increase code efficiency and make code easier to read.
+    
+  - Installing loadash: npm i loadash
+  - Docs: 
+*/
