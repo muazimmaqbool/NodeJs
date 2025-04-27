05e9d62b7d2ac16a3f8d48dc3af4b709495e3983
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-27 09:04:16 +0530

    get request server example

--- 14_ExpressJS.js
+++ 14_ExpressJS.js
@@ -15,13 +15,23 @@ const app = express() //'app' can be any name but generally all developers use '
 
 /* ->app.get takes to parameters:
       (app.get() is a method in Express used to define a route that listens for GET requests on a specific URL or endpoint.)
-      1)to which endpoint it needs to be shown  kon se endpoint pei ye show hona chahiye)   
+      1)to which endpoint it needs to be shown  kon se endpoint pei ye show hona chahiye) 
+        here endpoint is "/", i.e after address when the user types this "/", the user will get data
+
       2)a callback function that handles the request and response (req, res)
+          request->request
+          res->response: what response you want to send here it's "Hello Express"
+
 */
-//app.get(path, callback)+
+//app.get(path, callback)
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Welcome to my hotel!')
+})
+
+//another get request: localhost:3000/menu
+app.get('/menu',(req,res)=>{
+  res.send('We currently have: Pizza, Burger, Egg Roles, Briyani, Butter Chicken, Tandori Chicken')
 })
 
 //3000 is port
-app.listen(3000)
+app.listen(3000) // after running the code, go to web browser and type localhost:3000/
