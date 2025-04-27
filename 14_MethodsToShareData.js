1c5b35299a4d503813a4303c9a97bfd9435cab3d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-27 08:53:56 +0530

    get methods

--- 14_MethodsToShareData.js
+++ 14_MethodsToShareData.js
@@ -1,11 +1,14 @@
 /*
 ->Mehods to share data:
     -How data is send and received between a client(like a web browser) and a sever (here built with node.js)
-    -There are lot of methods out there to share and receive data according to their needs:
+    -There are some methods out there to share and receive data according to their needs:
         1)GET
         2)POST
         3)PATCH
         4)PUT
         5)DELETE
 
+    1)GET method: used to get request for some data/information from the server
+                  example: when you enter an websites url in the browser, the browser send the GET to the server to
+                           fetch the web page
 */
