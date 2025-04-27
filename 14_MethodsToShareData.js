51d06b97befc7d7d5550eee8fcb5f76573d17268
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-04-27 08:55:02 +0530

    remaining methods

--- 14_MethodsToShareData.js
+++ 14_MethodsToShareData.js
@@ -11,4 +11,16 @@
     1)GET method: used to get request for some data/information from the server
                   example: when you enter an websites url in the browser, the browser send the GET to the server to
                            fetch the web page
+                           
+    2) POST method: used to send some new data to the server
+                    example: when you fill a signup form and click submit, the browser sends a POST request with your details to the server
+
+    3) PATCH method: used to update some part of the existing data on the server
+                     example: when you edit your profile picture only, a PATCH request is sent to update just that part
+
+    4) PUT method: used to completely update/replace existing data on the server
+                   example: when you edit your entire profile information and save it, a PUT request replaces the old profile with the new one
+
+    5) DELETE method: used to delete some data from the server
+                      example: when you delete your account, a DELETE request is sent to remove your data from the server
 */
