eea2ac2cf165f4f23a0b19d459112237a7b04f37
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-05-17 16:46:26 +0530

    full schema of person

--- Modals/Person.js
+++ Modals/Person.js
@@ -8,5 +8,29 @@ const personSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true, //here required is true now 'name' has be entered in the database
+    },
+    age:{
+        type:Number
+    },
+    work:{
+        type:String,
+        enum:['chef','waiter','manager'], //now work will accept only these three values
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true, //means the email has to be unique in the entire database
+    },
+    address:{
+        type:String,
+    },
+    salary:{
+        type:Number,
+        required:true
     }
 })
