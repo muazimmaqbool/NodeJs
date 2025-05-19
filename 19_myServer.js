6024af41f7912b618a33ce007267efdc9ff1d229
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-05-19 23:07:30 +0530

    installing body parser

--- 19_myServer.js
+++ 19_myServer.js
@@ -5,7 +5,7 @@ const db=require('./db')
 
 //Learn about this in 20_Body_Parser.txt file
 const bodyParser = require('body-parser');
-app.use(bodyParser.json())
+app.use(bodyParser.json())//here it will convert the json to js object and save it inside: req.body
 
 //Model name Person now can be used to do all database work for makeing changes in Person
 const Person=require("./Modals/Person")
