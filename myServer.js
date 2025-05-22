6a0e76ba606a268b88b309d931d0998d8bddd950
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-05-22 22:58:53 +0530

    POST request setup

--- myServer.js
+++ myServer.js
@@ -14,6 +14,17 @@ app.get('/', (req, res) => {
   res.send('Hello Welcome to my hotel!...')
 })
 
+//Creating POST method:
+app.post('/person',(req,res)=>{
+  /*Important:
+  here we whoever send data to this route i.e /person , here the data which client sends needs to be saved in the database but before
+  that the data which is send via POST request and the data is processed by body parser and body parser saves it inside "req.body"
+  */
+ const data=req.body //Assuming the request body contains the person data
+
+})
+
+
 app.listen(3000,()=>{
   console.log("Server is listening on port 3000")
 })
