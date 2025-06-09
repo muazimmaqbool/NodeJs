896f302f23dadb3248fb0c896e1d5b8179200d06
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-09 23:14:37 +0530

    working paramterized api call

--- myServer.js
+++ myServer.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json()); //here it will convert the json to js object and sav
 
 //Model name Person now can be used to do all database work for makeing changes in Person
 const Person = require("./Modals/Person");
-const MenuItem=require("./Modals/MenuItem")
+const MenuItem = require("./Modals/MenuItem");
 
 app.get("/", (req, res) => {
   res.send("Hello Welcome to my hotel!...");
@@ -57,9 +57,9 @@ app.post("/person", (req, res) => {
 });
 */
 //Important:
- //using this above method from app.get() will show error like this: Model.prototype.save() no longer accepts a callback
-  //Check document (day-5)
-  /*this method using callback on save is removed by nodejs now we use async-await
+//using this above method from app.get() will show error like this: Model.prototype.save() no longer accepts a callback
+//Check document (day-5)
+/*this method using callback on save is removed by nodejs now we use async-await
   Because:  Whatactually callback does, callback is a function that is executed just after
   the execution of another main function, it means the callback will wait until its main function is not executed
   -Callbacks also make code but complicated
@@ -68,23 +68,23 @@ app.post("/person", (req, res) => {
   */
 
 //Asyn-await method
-app.post("/person",async(req,res)=>{
+app.post("/person", async (req, res) => {
   /*
     If the promise is resolved, the result of the promise is returned. If the
     promise is rejected, it throws an error that can be caught using
     try...catch
     */
-  try{
-    const data=req.body
-    const newPerson=new Person(data)
-    const response=await newPerson.save()
-   // console.log("Saved Person Data:",response)
-    res.status(200).json(response)
-  }catch(err){
-    console.error('Error saving person:', err);
-    res.status(500).json({ error: 'Internal server error' });
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
+    const response = await newPerson.save();
+    // console.log("Saved Person Data:",response)
+    res.status(200).json(response);
+  } catch (err) {
+    console.error("Error saving person:", err);
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 
 /*
 ->Now Post/save below data to this endpoint: http://localhost:3000/person in postman
@@ -116,47 +116,65 @@ Saved Person Data: {
 //now you can go to mongoDB compass, refress and you will see "hotels" database inside it will be "people" collection
 //and inside "people" you will find this data
 
-
 //Method to get person data (fetching person data from database)
-app.get("/person",async(req,res)=>{
-  try{
+app.get("/person", async (req, res) => {
+  try {
     //here what it will do is that it will return every record/data from "Person" collection
-    const data=await Person.find()
+    const data = await Person.find();
     //console.log("Data Fetched:",data)
-    res.status(200).json(data)
-  }catch(err){
-    console.log("Error occured while fetching the data:",err)
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(200).json(data);
+  } catch (err) {
+    console.log("Error occured while fetching the data:", err);
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 //now hit this url with GET method in postman: http://localhost:3000/person or in chrome (browser generally does GET request)
 
 //saving menuitems data
 //POST METHOD to save menu item
-app.post("/menuNew",async(req,res)=>{
-  try{
-    const data=req.body
-    const newMenu=new MenuItem(data)
-    const response=await newMenu.save()
-    console.log("Menu Saved:",response)
-    res.status(200).json(response)
-  }catch(err){
-    console.error('Error saving Menu:', err);
-    res.status(500).json({ error: 'Internal server error' });
+app.post("/menuNew", async (req, res) => {
+  try {
+    const data = req.body;
+    const newMenu = new MenuItem(data);
+    const response = await newMenu.save();
+    console.log("Menu Saved:", response);
+    res.status(200).json(response);
+  } catch (err) {
+    console.error("Error saving Menu:", err);
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 //Getting menu data: http://localhost:3000/menu
-app.get("/menu",async(req,res)=>{
-  try{
-    const data=await MenuItem.find()
-    res.status(200).json(data)
-  }catch(error){
-    console.log("Error while fetching menu:",error)
-    res.status(500).json({error:"Internal server error"})
+app.get("/menu", async (req, res) => {
+  try {
+    const data = await MenuItem.find();
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("Error while fetching menu:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
-})
+});
 
-//parametrised API call (see file 24_Parametrized_API_Calls.txt)
+//parametrised API call (see file 24_Parametrized_API_Calls.txt) to get person based on work type
+//here using :workType - makes it a variable means anything can be inplace of work
+app.get("/person/:workType", async (req, res) => {
+  try {
+    const workType = req.params.workType;
+    //checking for workType, so that user enters valid worktype, and only then db validation will be done
+    if (workType === "chef" || workType === "manager" || workType === "waiter") {
+      //db operation, it will look for the entered workType in the work field of person
+      const response = await Person.find({ work: workType });
+      //console.log("fetched...");
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({ error: "Invalid work type" });
+    }
+  } catch (err) {
+    console.log("Error while fetching work:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+//now test it like this: http://localhost:3000/person/chef, http://localhost:3000/person/manager,...
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
