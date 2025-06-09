d8633e450e0a7d4707e64f6569e062322336c90f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-09 23:01:45 +0530

    name fix

--- myServer.js
+++ myServer.js
@@ -156,7 +156,7 @@ app.get("/menu",async(req,res)=>{
   }
 })
 
-//parametrised API call (see file 24_Parametrized API Calls.txt)
+//parametrised API call (see file 24_Parametrized_API_Calls.txt)
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
