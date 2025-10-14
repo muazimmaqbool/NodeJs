02396a72c2e2c56931a1148f540f423165fa6398
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-14 21:00:44 +0530

    npm

--- 2npm.js
+++ 2npm.js
@@ -1,4 +1,4 @@
-const a=6655;
+const a = 6655;
 console.log(a);
 // node .\2npm.js  or node 2npm.js or node 2npm: type this to run this in terminal
 
@@ -8,35 +8,32 @@ console.log(a);
   nodejs has indefinite number of packages but we only need to know few search on google: best packages of npm/nodejs
   */
 
-  //to initilize npm  type: npm init then type app-name description etc others are optional
-  //now after completing it you will see package.js file in your folder
+//to initilize npm  type: npm init then type app-name description etc others are optional
+//now after completing it you will see package.js file in your folder
 
 //why this package.json file?
- // this file is used to keep track of records, like whenever we download any new package it will be added inside package.js
- // this file is used to keep track of our files/packages/depdencies in our project
+// this file is used to keep track of records, like whenever we download any new package it will be added inside package.js
+// this file is used to keep track of our files/packages/depdencies in our project
 
- //now downloading a package: npm intall packagename or npm i packagename
- //for now we will download express : its framework of nodejs  with the help of express we can do our work quickly
- // we don't use it now but later for now lets download it as an example: npm install or i express
+//now downloading a package: npm intall packagename or npm i packagename
+//for now we will download express : its framework of nodejs  with the help of express we can do our work quickly
+// we don't use it now but later for now lets download it as an example: npm install or i express
 
- // now you will see we got two more files in our folder package-lock.json and node-modules
- // -> package-lock.json : contains dependencies of dependency (i.e express is an dependencies it contains
- //    many dependencies and those are mentioned inside this folder package-lock.json)
- // -> node_modules: contains all of our dependencies (this file is important)
+// now you will see we got two more files in our folder package-lock.json and node-modules
+// -> package-lock.json : contains dependencies of dependency (i.e express is an dependencies it contains
+//    many dependencies and those are mentioned inside this folder package-lock.json)
+// -> node_modules: contains all of our dependencies (this file is important)
 
- //Note-> Here we don't need express so we will uninstall it -> npm uninstall express (or delete its files)
-         //now package-lock.json is empty
+//Note-> Here we don't need express so we will uninstall it -> npm uninstall express (or delete its files)
+//now package-lock.json is empty
 
- // -> A dependency is any external resource a program needs to work.
+// -> A dependency is any external resource a program needs to work.
 
- /*A framework is a foundation over which programmers can build their software. In a similar way,
+/*A framework is a foundation over which programmers can build their software. In a similar way,
      a JavaScript framework provides a base for the convenience of the programmers. 
     It helps by providing a set of pre-written codes so the programmers would not need to start from scratch.
     */
 
-/*A libraries include pre-written JavaScript code that simplifies the completion of common 
+/*A libraries include a pre-written JavaScript code that simplifies the completion of common 
 and complicated tasks. For example, DOM manipulation, framework setup, and AJAX handling are all common targets for JavaScript libraries.
     */
-
-
-
