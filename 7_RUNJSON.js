cae8ae1bcc34f6aacd7a4b1651617e70512fda97
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-17 22:13:44 +0530

    JOSN in nodejs

--- 7_RUNJSON.js
+++ 7_RUNJSON.js
@@ -1,15 +1,15 @@
 /*
- #17 importing and displaying json data via node
-  -> This file is used to run and show json data which is inside 7_Data.json  file
+ #17 Displaying json data via node
+  -> This file is used to run and show json data which is inside 7_Data.json file
 */
 
 /*
  Note:if you write 7_Data only then nodejs will first try to fint 7_Data.js before trying to find 7_Data.json
  So, if there is an 7_Data.js file in you folder then node.js will run it and will not run the actual json file so, 
- its better you use name with extension also like this filename.json 
+ it's better you use name with extension also like this filename.json 
 */
 const data=require("./7_Data.json")
-console.log(data)
+console.log("data",data)
 /*
 o/p: { name: 'Batman', address: { street: 'Wayne Manor', city: 'Gotham' } }
 ->This is the default behavior in nodejs, i.e:
